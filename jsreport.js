9df5970d17e087f1c96d8a0ca6acf8414a3bb31a
Emmanuel Potvin <epotvin@kogitec.com>
2015-07-27 12:48:11 -0400

    tests...

--- jsreport.js
+++ jsreport.js
@@ -1,9 +1,20 @@
 var toner = Npm.require("toner")();
+toner.engine("jsrender", Npm.require("toner-jsrender"));
+toner.recipe("phantom", Npm.require("toner-phantom")());
 
-Meteor.methods({
-    render: function () {
-        console.log('asdf');
-    }
+var express = Npm.require('express');
+var app = express();
+WebApp.connectHandlers.use(Meteor.bindEnvironment(app));
+
+app.get('/report', function (req, res) {
+    toner.render({
+        template: {
+            engine: "jsrender",
+            recipe: "phantom",
+            content: "<h1>{{:foo}}</h1>"
+        },
+        data: { foo: "hello world"}
+    }, function(err, out) {
+        out.stream.pipe(res);
+    });
 });
-
-console.log('started');
--- package.js
+++ package.js
@@ -1,17 +1,21 @@
 Package.describe({
     summary: "Exposes the jsrender engine from Meteor applications",
     version: "0.0.1",
-    name: "epotvin:jsrender",
-    git: "https://github.com/epotvin/meteor-jsrender.git",
+    name: "epotvin:jsreport",
+    git: "https://github.com/epotvin/meteor-jsreport.git",
 });
 
 Package.onUse(function (api) {
     api.versionsFrom('1.1.0.2');
-    api.addFiles(['jsreport.js']);
+
+    api.use('webapp', 'server');
+
+    api.addFiles(['jsreport.js'], 'server');
 });
 
 Npm.depends({
     "toner": "0.1.3",
     "toner-jsrender": "0.1.0",
-    "toner-phantom": "0.1.1"
+    "toner-phantom": "0.1.1",
+    "express": "4.13.1"
 });
